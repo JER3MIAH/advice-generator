382ede302337c63a6ea5049c8505d67ca152b612
JER3MIAH <jeremiahbrownie@gmail.com>
2024-07-22 09:09:06 +0100

    Clear repo

--- android/app/src/main/kotlin/com/example/porfolio_website/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.porfolio_website
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:porfolio_website/src/app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
